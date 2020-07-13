61c199ac72621effe18ab5c8a2ce3d4c4ab061a9
TimE <65202752+TimEismar@users.noreply.github.com>
2020-07-13 14:43:30 +0200

    Create 01_Datentypen_Console.dart

---
+++ 01_Datentypen_Console.dart
@@ -0,0 +1,15 @@
+void main() {
+  int int1 = 1;
+  int int2 = 999;
+  double doub1 = 5.1893392392;
+  double doub2 = 737328932.22222;
+  String str1 = "Test";
+  String str2 = "hsgjsj";
+  
+  print(int1);
+  print(int2);
+  print(doub1);
+  print(doub2);
+  print(str1);
+  print(str2);
+}
