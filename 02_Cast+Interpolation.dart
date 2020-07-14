e883a32fad41d92c93fc9a524318ddcdb5aa512e
TimE <65202752+TimEismar@users.noreply.github.com>
2020-07-14 10:28:36 +0200

    Create 02_Cast+Interpolation.dart

---
+++ 02_Cast+Interpolation.dart
@@ -0,0 +1,10 @@
+void main() {
+  String meinName = "Tim";
+  double doub = 3.1122112212;
+  String cast = doub.toString();
+  int laenge = meinName.length; 
+  
+  print("Hallo ich bin $meinName");
+  print(cast);
+  print(laenge); 
+}
