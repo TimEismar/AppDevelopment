0067e052133c2fc633bddcfb6da8dd14203e0bf7
TimE <65202752+TimEismar@users.noreply.github.com>
2020-07-14 10:53:02 +0200

    Create 03_Rechnen(Unäre Operatoren).dart

---
+++ 03_Rechnen(Unäre Operatoren).dart
@@ -0,0 +1,16 @@
+void main() {
+  int test = 13;
+  
+  test = test + 7;
+  test = test - 3;
+  
+  print (test);
+  
+  test++;
+  print(test);
+  
+  int zahl = 13;
+  zahl*= zahl;
+  print(zahl);
+  
+}
