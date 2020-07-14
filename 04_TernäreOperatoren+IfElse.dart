b7d95bf23af2baaca4c70d8bf468e98f28d436b8
TimE <65202752+TimEismar@users.noreply.github.com>
2020-07-14 11:16:35 +0200

    Create 04_TernäreOperatoren+IfElse.dart

---
+++ 04_TernäreOperatoren+IfElse.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+int zahlengrenze = 1;
+String ttt;
+  
+if(zahlengrenze < 10){
+  print("Kleiner");
+  zahlengrenze++;
+}
+else{
+  print("Größer");
+}
+  
+ttt = zahlengrenze > 10 ? "Yes sir" : "Negativ"; 
+print(ttt);  
+}
