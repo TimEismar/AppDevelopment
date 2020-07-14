675f5ceedd32080e1a2d7b7413f43584db9489ed
TimE <65202752+TimEismar@users.noreply.github.com>
2020-07-14 11:35:12 +0200

    Create 05_ForSchleife.dart

---
+++ 05_ForSchleife.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  int limit = -99;
+  
+ for(limit; limit < 5; limit++){
+    print(limit);
+  }
+  
+  int limit2 = 99;
+  
+ for(int i=0; i < limit2; i++){
+    print(i);
+  }
+}
